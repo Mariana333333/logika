8c755011fe380dde8bce17421b7050af48e4aa3c
DESKTOP-IPQ6RJU\Lenovo_PC <33>
2023-10-13 20:06:06 +0300

    jtiobjsr

---
+++ Logika/m4/u1/main.py
@@ -0,0 +1,20 @@
+from PIL import Image, ImageFilter
+
+with Image.open('Sonor-Drums-Drumset.jpg') as original:
+    print(original.size)
+    print(original.format)
+    print(original.mode)
+
+    bw_original = original.convert('L')
+    bw_original.show()
+
+    blur_original = original.filter(ImageFilter.BLUR)
+    blur_original.show()
+
+    left_original = original.transpose(Image.ROTATE_90)
+    left_original.show()
+
+    bw_original.save('bw.jpg')
+
+
+
