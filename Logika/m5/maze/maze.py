f09e03482c79de2488afc7cf08c70bc49b2c64f8
DESKTOP-IPQ6RJU\Lenovo_PC <33>
2023-12-01 20:01:06 +0200

    regetg

--- Logika/m5/maze/maze.py
+++ Logika/m5/maze/maze.py
@@ -42,6 +42,22 @@ class Enemy(GameSprite):
         if self.rect.x >= win_width-80:
             self.direction = 'left'
 
+class Wall(sprite.Sprite):
+    def __init__(self, wall_x, wall_y, wall_width, wall_height):
+        super().__init__()
+        self.width = wall_width
+        self.height = wall_height
+
+        self.image = Surface((self.width, self.height))
+        self.image.fill((0, 225, 0))
+
+        self.rect = self.image.get_rect()
+        self.rect.x = wall_x
+        self.rect.y = wall_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
 
 win_width = 700
 win_height= 500
@@ -53,7 +69,7 @@ background = scale(load("background.jpg"), (win_width, win_height))
 player = Player('hero.png', 5, win_height - 80, 4)
 monster = Enemy('cyborg.png', win_width - 150, win_height - 300, 2)
 final = GameSprite('treasure.png', win_width-80, win_height-80, 0)
-
+wall1 = Wall(0, 320, 100, 10)
 
 
 game = True
@@ -61,9 +77,21 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
+font.init()
+
+font = font.Font(None, 70)
+
+win = font.render('VITCTORY!!!', True, (60, 67, 200))
+lose = font.render('GAME OVER!!!', True, (250, 0, 10))
+
+
 mixer.init()
 mixer.music.load("jungles.ogg")
 mixer.music.play(-1)
+
+
+money_sound = mixer.Sound('money.ogg')
+kick_sound = mixer.Sound('kick.ogg')
 
 while game:
     for e in event.get():
@@ -75,9 +103,22 @@ while game:
         player.reset()
         monster.reset()
         final.reset()
+        wall1.reset()
 
         player.update()
         monster.update()
+
+        if sprite.collide_rect(player, final):
+            finish = True
+            window.blit(win, (200, 200))
+            money_sound.play()
+
+        if sprite.collide_rect(player, monster) or sprite.collide_rect(player, wall1):
+            finish = True
+            window.blit(lose, (200, 200))
+            kick_sound.play()
+
+
 
     display.update()
     clock.tick(FPS)
